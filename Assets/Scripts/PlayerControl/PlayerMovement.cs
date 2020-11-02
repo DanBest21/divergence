2b63c598eb3203512602c6a277d6a02c9e1caed0
Ben <benjaminjbeasley@gmail.com>
2020-11-02 19:49:03 +0000

    Simple player movement

---
+++ Assets/Scripts/PlayerControl/PlayerMovement.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    [SerializeField]
+    private Camera mainCamera = null;
+
+    private Vector2 input;
+
+    [SerializeField]
+    private float speed = 3;
+
+    private void Awake ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        input = new Vector2(
+            Input.GetAxisRaw("Horizontal"), 
+            Input.GetAxisRaw("Vertical")
+            );
+
+        if(input.sqrMagnitude > 1)
+        {
+            input = input.normalized;
+        }
+
+        UpdateLookRotation();
+    }
+
+    void UpdateLookRotation ()
+    {
+        Vector3 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+
+        float angle = Vector2.SignedAngle(Vector2.up, mousePos - transform.position);
+
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    private void FixedUpdate ()
+    {
+        rb.velocity = input * speed;
+    }
+}
