5b3c96151172f1c1e0156d28864837d9cce167f2
db5n17 <db5n17@soton.ac.uk>
2020-11-16 05:35:20 +0000

    Finished initial pass at implementing basic FOV.

---
+++ Assets/Scripts/Editor/FieldOfViewEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor (typeof (FieldOfView))]
+public class FieldOfViewEditor : Editor
+{
+    private void OnSceneGUI()
+    {
+        FieldOfView fov = (FieldOfView)target;
+        Handles.color = Color.white;
+        Handles.DrawWireDisc(fov.transform.position, Vector3.forward, fov.ViewRadius());
+
+        Vector3 initialAngle = fov.CalculateAngle(-fov.ViewAngle() / 2, false);
+        Vector3 endAngle = fov.CalculateAngle(fov.ViewAngle() / 2, false);
+        Handles.DrawLine(fov.transform.position, fov.transform.position + initialAngle * fov.ViewRadius());
+        Handles.DrawLine(fov.transform.position, fov.transform.position + endAngle * fov.ViewRadius());
+
+        Handles.color = Color.green;
+
+        foreach (Transform point in fov.VisiblePoints())
+        {
+            Handles.DrawLine(fov.transform.position, point.position);
+        }
+    }
+}
---
+++ Assets/Scripts/FieldOfView/FieldOfView.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    [SerializeField]
+    private float viewRadius;
+    [SerializeField]
+    [Range(0, 360)]
+    private float viewAngle;
+
+    [SerializeField]
+    private LayerMask interestPoints;
+    [SerializeField]
+    private LayerMask obstructions;
+
+    [SerializeField]
+    private List<Transform> visiblePoints = new List<Transform>();
+
+    public float ViewRadius() { return viewRadius; }
+
+    public float ViewAngle() { return viewAngle; }
+
+    public LayerMask InterestPoints() { return interestPoints; }
+
+    public LayerMask Obstructions() { return obstructions; }
+
+    public List<Transform> VisiblePoints() { return visiblePoints; }
+
+    void Start()
+    {
+        StartCoroutine("FindPointsCoroutine", 0.2f);
+    }
+
+    IEnumerator FindPointsCoroutine(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisiblePoints();
+        }
+    }
+
+    void FindVisiblePoints()
+    {
+        visiblePoints.Clear();
+
+        Collider2D[] pointsInCircle = Physics2D.OverlapCircleAll(transform.position, viewRadius, interestPoints);
+            
+        foreach (Collider2D point in pointsInCircle)
+        {
+            Vector3 directionToPoint = (point.transform.position - transform.position).normalized;
+
+            if (Vector2.Angle(transform.up, directionToPoint) <= viewAngle / 2)
+            {
+                float distanceToPoint = Vector3.Distance(transform.position, point.transform.position);
+
+                if (!Physics2D.Raycast(transform.position, directionToPoint, distanceToPoint, obstructions))
+                {
+                    visiblePoints.Add(point.transform);
+                }
+            }
+        }
+    }
+
+    public Vector3 CalculateAngle(float angle, bool isGlobal)    
+    {
+        if (!isGlobal)
+        {
+            angle -= transform.eulerAngles.z;
+        }
+        
+        return new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad), Mathf.Cos(angle * Mathf.Deg2Rad), 0);
+    }
+}
