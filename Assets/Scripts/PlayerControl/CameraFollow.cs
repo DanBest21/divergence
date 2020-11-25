f87bf8b1d93d9d237b9cf67b9f57f38ae8b9bad0
Ben <benjaminjbeasley@gmail.com>
2020-11-25 19:09:09 +0000

    Quick camera follow

---
+++ Assets/Scripts/PlayerControl/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    private Transform target = null;
+
+    [SerializeField]
+    private float smoothTime = 1f;
+
+    private Vector3 velocity = Vector3.zero;
+
+    void LateUpdate()
+    {
+        Vector3 targetPos = target.position;
+        targetPos.z = transform.position.z;
+
+        transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
+    }
+}
